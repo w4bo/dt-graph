5c8b243421a096eabe3b1e0d2bc93e6bffe07db6
Matteo Francia <m.francia@unibo.it>
2025-03-25 15:18:17 +0100

    merge, tests are broken, missing geometry deserialization

--- src/test/kotlin/TestKotlin.kt
+++ src/test/kotlin/TestKotlin.kt
@@ -428,11 +428,12 @@ class TestKotlin {
         val p6 = g.addProperty(e1.id.toLong(), "p6", 1L, PropType.LONG, sourceType = EDGE)
         val p7 = g.addProperty(e1.id.toLong(), "p7", 1.0, PropType.DOUBLE, sourceType = EDGE)
         val p8 = g.addProperty(e1.id.toLong(), "location", "{\"coordinates\":[11.799328,44.235394],\"type\":\"Point\"}", PropType.GEOMETRY, sourceType = EDGE)
+        val p9 = g.addProperty(e1.id.toLong(), "p9", "A".repeat(MAX_LENGTH_VALUE), PropType.STRING, sourceType = EDGE)
         g.flushToDisk()
         assertEquals(g.getNode(N0), g.getNodeFromDisk(N0))
         assertEquals(n1, g.getNodeFromDisk(N1))
         assertEquals(e1, g.getEdgeFromDisk(e1.id.toLong()))
-        listOf(p1, p2, p3, p4, p5, p6, p7, p8).forEach {
+        listOf(p1, p2, p3, p4, p5, p6, p7, p8, p9).forEach {
             assertEquals(it, g.getPropertyFromDisk(it.id.toLong()))
         }
     }
