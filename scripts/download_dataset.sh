a3d7aa7a2c583adbb5f735b0a9a7871064132ee0
mfrancia <m.francia@unibo.it>
2025-08-28 14:44:16 +0200

    integrating the scripts

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
+set -exo
 
 DATASET_SIZE=$1
-# MEGA link (not OneDrive!)
 LINK="https://big.csr.unibo.it/downloads/stgraph/stgraph/"
 INTERNAL_LINK="137.204.74.24/downloads/stgraph/stgraph/"
+OUTPUT_DIR="${2:-"/dt_graph/src/main/resources/dataset/smartbench"}"
 
-OUTPUT_DIR="/dt_graph/src/main/resources/dataset/smartbench"
-
-mkdir -p "$OUTPUT_DIR"
-
-echo "Downloading dataset ..."
-cd "$OUTPUT_DIR" || exit 1
-
+# curl -L -o "./${DATASET_SIZE}.tar" "${LINK}${DATASET_SIZE}.tar"
 if ! wget --no-check-certificate --tries=3 "${LINK}${FILENAME}"; then
     echo "Primary link failed, trying backup..."
     wget --no-check-certificate --tries=3 "${INTERNAL_LINK}${FILENAME}" || {
