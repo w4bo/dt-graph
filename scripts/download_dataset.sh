f642ee7762c687dca134172cbc77168d354ccc46
mfrancia <m.francia@unibo.it>
2025-08-28 14:49:29 +0200

    fix the download script

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -6,6 +6,10 @@ LINK="https://big.csr.unibo.it/downloads/stgraph/stgraph/"
 INTERNAL_LINK="137.204.74.24/downloads/stgraph/stgraph/"
 OUTPUT_DIR="${2:-"/dt_graph/src/main/resources/dataset/smartbench"}"
 
+mkdir -p "$OUTPUT_DIR"
+cd "$OUTPUT_DIR" || exit 1
+
+echo "Downloading dataset ..."
 # curl -L -o "./${DATASET_SIZE}.tar" "${LINK}${DATASET_SIZE}.tar"
 if ! wget --no-check-certificate --tries=3 "${LINK}${FILENAME}"; then
     echo "Primary link failed, trying backup..."
