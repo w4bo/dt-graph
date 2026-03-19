200bd1809647971e7630c9db881849ae88520a2b
w4bo <m.francia@unibo.it>
2026-03-19 10:06:23 +0100

    allow remote gradle check

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,14 @@ tasks.withType<Test>().configureEach {
      javaLauncher = javaToolchains.launcherFor {
          languageVersion = JavaLanguageVersion.of(jVersion)
      }
+    jvmArgs = listOf(
+        "-Dcom.sun.management.jmxremote",
+        "-Dcom.sun.management.jmxremote.port=9010",
+        "-Dcom.sun.management.jmxremote.rmi.port=9010",
+        "-Dcom.sun.management.jmxremote.authenticate=false",
+        "-Dcom.sun.management.jmxremote.ssl=false",
+        "-Djava.rmi.server.hostname=172.27.236.32"
+    )
     testLogging {
         events("passed", "skipped", "failed")
     }
